a99af4bf7099d319035e342b3b83379523a75ed5
Anurav-Garg <anurav.grg@gmail.com>
2022-12-25 14:31:16 +0530

    Fixed form selection not updating

--- components/music/musicSurvey.js
+++ components/music/musicSurvey.js
@@ -3,43 +3,45 @@ import { useState } from "react";
 import { MusicForm } from "./musicForm";
 
 export function MusicSurvey() {
-  const SubmissionHandler = function () {
-    if (selection === "yes") {
-      setContents(() => <div>{"+50 Social Credit"}</div>);
-    } else {
-      setContents(() => (
-        <div>
-          {
-            "Thank you for your feedback! We will get back to you at your physical location in 3-5 business days 😇"
-          }
-        </div>
-      ));
-    }
-  };
-
-  const [selection, setSelection] = useState("no");
+  const [formSubmission, setFormSubmission] = useState("");
 
   const SelectionHandler = (value) => {
-    // console.log(value);
     setSelection(value);
   };
 
-  const [Contents, setContents] = useState(() => {
+  const SubmissionHandler = function () {
+    setFormSubmission(selection);
+  };
+
+  const [selection, setSelection] = useState("yes");
+
+  if (formSubmission === "") {
     return (
-      <>
+      <Section heading="Your Thoughts: Survey">
         <div>{`It goes without saying really but I'm sure by now you're too
-      convinced that Library of Ruina has the greatest OST ever conceived.
-      To be sure, though, we are hosting a survey, and would like you to
-      fill in the form below as truthfully as possible! 😇`}</div>
+  convinced that Library of Ruina has the greatest OST ever conceived.
+  To be sure, though, we are hosting a survey, and would like you to
+  fill in the form below as truthfully as possible! 😇`}</div>
 
         <MusicForm
           selection={selection}
           selectionHandler={SelectionHandler}
           submissionHandler={SubmissionHandler}
         />
-      </>
+      </Section>
     );
-  });
+  }
 
-  return <Section heading="Your Thoughts: Survey">{Contents}</Section>;
+  return (
+    <Section heading="Your Thoughts: Survey">
+      {formSubmission === "yes" && <div>{"+50 Social Credit"}</div>}
+      {formSubmission === "no" && (
+        <div className="pb-44">
+          {
+            "Thank you for your feedback! We will get back to you at your physical location in 3-5 business days 😇"
+          }
+        </div>
+      )}
+    </Section>
+  );
 }
