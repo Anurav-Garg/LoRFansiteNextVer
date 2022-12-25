1a7b724670b7292bd5d4c402a7786a62d2c3030d
Anurav-Garg <anurav.grg@gmail.com>
2022-12-25 15:42:00 +0530

    Made content-container width dynamic

--- components/common/contentContainer.js
+++ components/common/contentContainer.js
@@ -1,6 +1,6 @@
 export function ContentContainer(props) {
   return (
-    <div className="z-0 text-slate-300 text-xl max-w-6xl mx-auto py-8">
+    <div className="z-0 text-slate-300 text-xl w-4/5 mx-auto py-8">
       {props.children}
     </div>
   );
