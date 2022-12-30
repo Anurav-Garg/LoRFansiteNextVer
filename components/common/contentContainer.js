657754df539c3bb57457f2b6575c02a1baf656a0
Anurav-Garg <anurav.grg@gmail.com>
2022-12-30 20:46:03 +0530

    Updated common component sizes for mobile mode

--- components/common/contentContainer.js
+++ components/common/contentContainer.js
@@ -1,6 +1,6 @@
 export function ContentContainer(props) {
   return (
-    <div className="z-0 text-slate-300 text-xl w-4/5 mx-auto py-8">
+    <div className="z-0 text-slate-300 text-xl w-5/6 md:w-4/5 mx-auto py-8">
       {props.children}
     </div>
   );
--- components/common/section.js
+++ components/common/section.js
@@ -1,7 +1,9 @@
 export function Section(props) {
   return (
     <>
-      <h2 className="text-4xl text-yellow-400 pt-8 pb-2">{props.heading}</h2>
+      <h2 className="text-3xl md:text-4xl text-yellow-400 pt-8 pb-2">
+        {props.heading}
+      </h2>
       {props.children}
     </>
   );
