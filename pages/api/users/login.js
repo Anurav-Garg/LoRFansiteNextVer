078eefe86ce3031e168e90dd22f312cc7ac9facd
Anurav-Garg <anurav.grg@gmail.com>
2023-01-03 15:00:57 +0530

    Added login and register API endpoints

---
+++ pages/api/users/login.js
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  try {
+    if (req.method === "POST") {
+      const { username, password } = JSON.parse(req.body);
+
+      if (!(username && password)) {
+        res
+          .status(400)
+          .json({ message: "Both Username and Password are required" });
+        return;
+      }
+
+      const existingUser = await prisma.user.findUnique({
+        where: { username: username },
+      });
+      if (!existingUser) {
+        res
+          .status(400)
+          .json({ message: `No user found with username "${username}"` });
+        return;
+      }
+
+      if (await compare(password, existingUser.password)) {
+        const token = sign({ username: username }, process.env.TOKEN_KEY, {
+          expiresIn: "2h",
+        });
+
+        res.status(200).json({ username: username, token: token });
+        return;
+      } else {
+        res.status(400).json({ message: "Incorrect password" });
+        return;
+      }
+    } else {
+      res.status(400).json({ message: "Request must be a POST request" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
---
+++ pages/api/users/register.js
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+import { hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  try {
+    if (req.method === "POST") {
+      const { username, password } = JSON.parse(req.body);
+
+      if (!(username && password)) {
+        res
+          .status(400)
+          .json({ message: "Both Username and Password are required" });
+        return;
+      }
+
+      const existingUser = await prisma.user.findUnique({
+        where: { username: username },
+      });
+      if (existingUser) {
+        res.status(400).json({ message: "Username taken" });
+        return;
+      }
+
+      const hashedPassword = await hash(password, 10);
+
+      const user = await prisma.user.create({
+        data: {
+          username: username,
+          password: hashedPassword,
+        },
+      });
+
+      const token = sign({ username: user.username }, process.env.TOKEN_KEY, {
+        expiresIn: "2h",
+      });
+      // TEMP:
+      console.log(token);
+
+      res.status(201).json({ username: user.username, token: token });
+    } else {
+      res.status(400).json({ message: "Request must be a POST request" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
