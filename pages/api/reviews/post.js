c5cb370cfd7a43b8bafa178f1a200453506b82ea
Anurav-Garg <anurav.grg@gmail.com>
2023-01-04 14:25:57 +0530

    Finished review post api functionality

--- pages/api/reviews/post.js
+++ pages/api/reviews/post.js
@@ -7,7 +7,6 @@ export default async function handler(req, res) {
   try {
     if (req.method === "POST") {
       // TEMP:
-      // console.log(req.headers);
       // res.setHeader("yeah", "indeed!");
       // res.status(200).json();
       if (!req.headers.authorization) {
@@ -22,9 +21,42 @@ export default async function handler(req, res) {
       }
       try {
         const decoded = verify(token, process.env.TOKEN_KEY);
+
+        const existingReview = await prisma.review.findUnique({
+          where: { authorId: decoded.id },
+        });
+
+        if (existingReview) {
+          res.status(400).json({ message: "Review by user already exists" });
+          return;
+        }
+
+        const { recommended, title, text } = JSON.parse(req.body);
         // TEMP:
-        
-        res.status(200).json();
+        console.log(JSON.parse(req.body));
+
+        if (!(recommended && title && text)) {
+          res
+            .status(400)
+            .json({ message: "All review parameters are required" });
+          return;
+        }
+
+        const recommendedBool = recommended.toLowerCase() === "true";
+        const review = await prisma.review.create({
+          data: {
+            recommended: recommendedBool,
+            title: title,
+            text: text,
+            author: {
+              connect: {
+                id: decoded.id,
+              },
+            },
+          },
+        });
+
+        res.status(201).json(review);
         return;
       } catch (err) {
         res.status(403).json({ message: "Invalid token" });
